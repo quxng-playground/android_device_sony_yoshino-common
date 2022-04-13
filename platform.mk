0e9f348a43cb08690577077cc5c19981eb4ffd0e
Arian <arian.kulmer@web.de>
2022-04-14 00:12:50 +0200

    yoshino-common: Don't explicitly build fs_config_files
    The fs_config_files for each partition is already added to
    PRODUCT_PACKAGES by base_{product,system,system_ext,vendor}.mk.
    
    Change-Id: Ifbadacd5cb20beca70ed8b3accb3baf86a989c92

--- platform.mk
+++ platform.mk
@@ -33,7 +33,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # For android_filesystem_config.h permissions
 PRODUCT_PACKAGES += \
-    fs_config_files \
     fs_config_dirs
 
 # Enable dynamic partition size
