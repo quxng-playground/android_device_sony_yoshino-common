44777935875cd224ea4acf5e02392a1a9b3563c0
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    yoshino-common: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -114,7 +114,8 @@ TARGET_USES_ION := true
 ### HIDL
 BOARD_VNDK_VERSION := current
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 ifneq ($(filter poplar_dsds maple_dsds, $(TARGET_DEVICE)),)
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest_dsds.xml
 else
