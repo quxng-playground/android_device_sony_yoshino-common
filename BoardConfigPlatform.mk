dcd97cd1a565fd83fb656f07781719a67aa23531
Quang Anh <quxngg@gmail.com>
2026-03-24 02:51:58 +0000

    yoshino-common: Import YAAP FCM

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -105,7 +105,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(PLATFORM_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/yaap/config/device_framework_matrix.xml
 ifneq ($(filter poplar_dsds maple_dsds, $(TARGET_DEVICE)),)
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest_dsds.xml
 else
